89633e3a73eaa27911550d5404013349d9cf4ccc
magomedov <magomedov.michael@gmail.com>
2019-09-14 19:30:05 +0300

    first commit

---
+++ src/main/java/com/api/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String test() {
+        return "hello";
+    }
+}
